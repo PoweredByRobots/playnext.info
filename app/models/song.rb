d47a858fd11f24a956a85e4c2cefd028d9a6c626
PoweredByRobots <robots@bitsurf.org>
2019-02-21 20:29:44 -0800

    song: add key validations

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,32 @@
 class Song < ApplicationRecord
   has_many :branches
-
+  
   alias branch branches
+
+  validates_presence_of :artist, :title
+  validate :valid_key_number?, :valid_key_letter?
+
+  private
+
+  def key
+    "#{key_number}#{key_letter}"
+  end
+
+  def valid_key?
+    valid_key_number? && valid_key_letter?
+  end
+
+  def valid_key_number?
+    return if empty_key? || key_number.between?(1, 12)
+    errors.add(:key_number, 'out of range (1-12)')
+  end
+
+  def valid_key_letter?
+    return if empty_key? || key_letter.to_s.match?(/([a-b]|[A-B])/)
+    errors.add(:key_letter, 'Should be A or B')
+  end
+
+  def empty_key?
+    key_letter.nil? && key_number.nil?
+  end
 end
