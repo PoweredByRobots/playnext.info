a37cfb5a61dd51c643d5b34742e5dcc39e55a6ee
PoweredByRobots <robots@bitsurf.org>
2019-02-21 22:04:47 -0800

    track username

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -2,6 +2,7 @@ class AccessController < ApplicationController
   before_action :confirm_logged_in, except: [:login, :attempt_login, :logout]
 
   def menu
+    @username = session[:username]
   end
 
   def login
@@ -9,12 +10,13 @@ class AccessController < ApplicationController
 
   def attempt_login
     if params[:name].present? && params[:password].present?
-      found_user = AdminUser.where(name: params[:name]).first
+      found_user = User.where(name: params[:name]).first
       authorized_user = found_user.authenticate(params[:password]) if found_user
     end
 
     if authorized_user
       session[:user_id] = authorized_user.id
+      session[:username] = authorized_user.name
       flash[:notice] = 'logged in.'
       redirect_to(admin_path)
     else
@@ -25,6 +27,7 @@ class AccessController < ApplicationController
 
   def logout
     session[:user_id] = nil
+    session[:username] = nil
     flash[:notice] = 'logged out'
     redirect_to(access_login_path)
   end
