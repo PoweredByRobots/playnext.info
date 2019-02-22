d9e4bf4c00a513167b7aa2d4f9a69852e27042f2
PoweredByRobots <robots@bitsurf.org>
2019-02-21 22:05:24 -0800

    cleanup key validation, add bpm validation

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,19 +1,27 @@
 class Song < ApplicationRecord
+  attr_reader :artist, :title, :year, :key, :bpm, :energy
+
   has_many :branches
-  
+
   alias branch branches
 
   validates_presence_of :artist, :title
-  validate :valid_key_number?, :valid_key_letter?
-
-  private
+  validate :valid_key?, :valid_bpm?
 
   def key
     "#{key_number}#{key_letter}"
   end
 
+  private
+
   def valid_key?
-    valid_key_number? && valid_key_letter?
+    valid_key_number?
+    valid_key_letter?
+  end
+
+  def valid_bpm?
+    return if bpm.nil?
+    errors.add(:bpm, 'out of range (30 - 255)') unless bpm.between?(30, 255)
   end
 
   def valid_key_number?
