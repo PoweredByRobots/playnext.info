a935f32430b5026b5992a7c3c3f7e34cb6586b48
PoweredByRobots <robots@bitsurf.org>
2019-02-22 17:40:05 -0800

    Establish routing for main page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  root 'songs#search'
+  
   get 'admin', to: 'access#menu'
+  get 'songs', to: 'songs#search'
+  get 'songs/search'
   get 'access/menu'
   get 'access/login'
   get 'access/logout'
+
   post 'access/attempt_login'
 
   resources :songs
