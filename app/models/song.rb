fce0fbe4419e6c1df60848ce9103d0d53cb58577
PoweredByRobots <robots@bitsurf.org>
2019-05-07 22:35:33 -0700

    better song key validations

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,10 +1,11 @@
+require 'pry'
 class Song < ApplicationRecord
   has_many :branches
 
   alias branch branches
 
   validates_presence_of :artist, :title, :filename
-  validate :valid_key?, :valid_bpm?
+  validate :valid_bpm?, :valid_key? 
 
   def key
     "#{key_number}#{key_letter}"
@@ -13,23 +14,28 @@ class Song < ApplicationRecord
   private
 
   def valid_key?
-    valid_key_number?
-    valid_key_letter?
+    valid_key_number? && valid_key_letter?
   end
 
   def valid_bpm?
     return if bpm.nil?
-    errors.add(:bpm, 'out of range (30 - 255)') unless bpm.between?(30, 255)
+    return true if bpm.between?(30, 255)
+    errors.add(:bpm, 'out of range (30 - 255)')
+    false
   end
 
   def valid_key_number?
-    return if empty_key? || key_number.between?(1, 12)
+    return if empty_key?
+    return true if key_number.between?(1, 12)
     errors.add(:key_number, 'out of range (1-12)')
+    false
   end
 
   def valid_key_letter?
-    return if empty_key? || key_letter.to_s.match?(/([a-b]|[A-B])/)
-    errors.add(:key_letter, 'Should be A or B')
+    return if empty_key?
+    return true if key_letter.to_s.match?(/([a-b]|[A-B])/)
+    errors.add(:key_letter, 'should be A or B')
+    false
   end
 
   def empty_key?
